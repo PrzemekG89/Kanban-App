3591fed3bc0de9e7e06717d76620782c03f5bb4f
PrzemekG89 <31968780+PrzemekG89@users.noreply.github.com>
2018-02-09 21:43:23 +0100

    23,4

---
+++ server/models/lane.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const laneSchema = new Schema({
+    name: { type: 'String', required: true },
+    notes: [{ type: Schema.ObjectId, ref: 'Note', required: true }],
+    id: { type: 'String', required: true, unique: true },
+}, {
+        usePushEach: true
+    }
+);
+
+function populateNotes(next) {
+    this.populate('notes');
+    next();
+}
+
+function deleteNotes(next) {
+    const notes = this.notes;
+    notes.forEach(element => {
+        Note.findByIdAndRemove(element._id).exec()
+    });
+    next();
+}
+
+laneSchema.pre('find', populateNotes);
+laneSchema.pre('findOne', populateNotes);
+laneSchema.pre('remove', deleteNotes);
+
+export default mongoose.model('Lane', laneSchema);
